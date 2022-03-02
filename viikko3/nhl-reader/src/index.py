79f79422f6b4d8ade54366245eda3a5fae995955
Sini <siniesofia@protonmail.com>
2022-03-02 18:04:54 +0200

    tehtävät 1 ja 2 tehty

--- viikko3/nhl-reader/src/index.py
+++ viikko3/nhl-reader/src/index.py
@@ -1,6 +1,9 @@
 import requests
 from player import Player
 
+def sorting(player):
+    return player.points
+
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players"
     response = requests.get(url).json()
@@ -8,20 +11,36 @@ def main():
     print("JSON-muotoinen vastaus:")
     print(response)
 
-    players = []
+    finnish_players = []
 
     for player_dict in response:
-        player = Player(
-            player_dict['name']
-        )
+        if player_dict['nationality'] == 'FIN':
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['nationality'],
+                player_dict['goals'],
+                player_dict['assists']
+            )
 
-        players.append(player)
+            finnish_players.append(player)
 
-    print("Oliot:")
-    print("toimii")
 
-    for player in players:
+    print("Players from FIN:")
+
+    lista = []
+
+    for player in finnish_players:
+        print(player.points)
+
+    for player in finnish_players:
+        lista.append((player.points, player.name))
+
+    lista.sort(reverse=True)
+
+    for player in lista:
         print(player)
+
 
 if __name__ == "__main__":
     main()
--- viikko3/nhl-reader/src/player.py
+++ viikko3/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, team, nationality, goals, assists):
         self.name = name
-    
+        self.nationality = nationality
+        self.goals = goals
+        self.assists = assists
+        self.points = goals + assists
+
+
     def __str__(self):
-        return self.name
+        return f"{self.name:25}{self.nationality:5}{self.points}"
