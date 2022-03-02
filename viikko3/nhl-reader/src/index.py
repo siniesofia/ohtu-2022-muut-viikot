ebf1deb820ed92d37e1c7a46dd7d1120f6c7a8f1
Sini <siniesofia@protonmail.com>
2022-03-02 17:10:20 +0200

    alku

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,27 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        player = Player(
+            player_dict['name']
+        )
+
+        players.append(player)
+
+    print("Oliot:")
+    print("toimii")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
